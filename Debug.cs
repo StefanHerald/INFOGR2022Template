33df44452c380547c80de8d36be534d12b60c12e
E-Meister2012 <102592032+E-Meister2012@users.noreply.github.com>
2022-05-27 21:38:59 +0200

    Debug
    Added a debug screen
    Added lines to the debug screen
    Added the circles to the debug screen (yay!)

--- Debug.cs
+++ Debug.cs
@@ -1,14 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenTK;
+using System;
 using Template;
-
+using System.Linq;
 namespace INFOGR2022Template
 {
     internal class Debug
-    {
-        internal Surface debugScreen;
-    }
+	{
+		// member variables
+		Raytracer raytracer;
+		Camera camera;
+		Scene debugscene;
+		internal bool debugMode;
+		// initialize
+		internal Surface debugScreen;
+		float x1;
+		float x2;
+		float y1;
+		float y2;
+
+		public void Init()
+
+		{
+			//setting the scene, the camera and the ray tracer (not sure if the values are correct)
+			debugscene = new Scene();
+			camera = new Camera(new Vector3(0), new Vector3(0, 0, 1), new Vector3(0, 1, 0));
+			raytracer = new Raytracer(debugscene, camera);
+			debugScreen.Line(0, 0, 100, 100, 0x00FF00);
+		}
+		public void Tick()
+        {
+			//Clears the debug screen, then renders it
+			debugScreen.Clear(0x000000);
+			raytracer.Render();
+			//Draws the lines for the debug screen (could be looked at again, not sure)
+			for(int i = -debugScreen.width / 5; i <= debugScreen.width * 1.2; i+= 64)
+			debugScreen.Line(debugScreen.width / 2, debugScreen.height, i, 0, 0x00FF00);
+			//Gets all the spheres in the list of primitives
+			var circles = from a in MyApplication.scene.objects.OfType<Sphere>() select a;
+			//For every circle, the position is calculated 100 times using a formula
+			foreach (var c in circles)
+			{
+				for (double i = 0; i < 2 * Math.PI; i+= Math.PI / 50)
+				{
+					if (i == 0)
+                    {
+						x1 = (c.Position.X + (c.Radius * (float)Math.Cos(i))) * debugScreen.width / 10 + c.Position.X;
+						y1 = (c.Position.Z + (c.Radius * (float)Math.Sin(i))) * debugScreen.width / 10 + c.Position.Z;
+					}
+					//x2 and y2 are the previous positions, they make the lines possible
+					y2 = y1;
+					x2 = x1;
+                    x1 = (c.Position.X + (c.Radius * (float)Math.Cos(i))) * debugScreen.width / 10 + c.Position.X;
+                    y1 = (c.Position.Z + (c.Radius * (float)Math.Sin(i))) * debugScreen.width / 10 + c.Position.Z;
+                    debugScreen.Line((int)x1, (int)y1, (int)x2, (int)y2, 0xFFFFFF);
+				}
+			}
+		}
+
+	}
 }
--- MyApplication.cs
+++ MyApplication.cs
@@ -1,4 +1,3 @@
-
 using INFOGR2022Template;
 using OpenTK;
 using System;
@@ -11,17 +10,19 @@ namespace Template
 		internal Surface screen;
 		Raytracer raytracer;
 		Camera camera;
-		Scene scene;
+		internal static Scene scene;
 		internal bool debugMode;
 		// initialize
 		public void Init()
 		{
 			//setting the scene, the camera and the ray tracer (not sure if the values are correct)
 			scene = new Scene();
-			scene.objects.Add(new Sphere(new Vector3(0,0,4), 1, new Vector3(0f,0.5f,0.5f)));
+			scene.objects.Add(new Sphere(new Vector3(3,0,4), 2, new Vector3(0f,0.5f,0.5f)));
+			scene.objects.Add(new Sphere(new Vector3(2, 0, 3), 1, new Vector3(0f, 0.5f, 0.5f)));
 			scene.lights.Add(new Light(new Vector3(0,1,0), new Vector3(1, 1, 1)));
 			camera = new Camera(new Vector3(0), new Vector3(0,0, 1), new Vector3(0,1,0));
 			raytracer = new Raytracer(scene, camera);
+			screen.Line(0, 0, 100, 100, 0x0000FF);
 		}
 
 		// tick: renders one frame
@@ -29,6 +30,7 @@ namespace Template
 		{
 			screen.Clear(0x000000);
 			raytracer.Render();
+			screen.Line(0, 0, 100, 100, 0x0000FF);
 			if (debugMode) 
 			{
 				//this is the only way to get this to work properly (Eke, I really tried)
